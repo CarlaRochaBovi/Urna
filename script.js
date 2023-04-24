50bea41018bff154d1174c6ccc7c38f204b351a2
Carla Rocha Bovi01~ <caralarochabovi@gmail.com>
2023-04-24 19:34:57 -0300

    novas chapas

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
 
 let objeto = JSON.parse(localStorage.getItem('chapa')) || {
-    primeiraChapa: 0,
-    segundaChapa: 0
+    fenix: 0,
+    rep: 0,
+    fazerADiferenca: 0,
+    estudantesEmAcao: 0,
+    lionBlack: 0
+
 }
 
 
@@ -19,12 +23,13 @@ const btn = document.getElementById('btn')
 const input = document.getElementById('input')
 let expression =  ''
 function calculate(num) {
-    expression = num
+    expression += num
     input.value = expression
 }
 
 function erase() {
-    input.value = 0
+    expression = ''
+    input.value = expression
 }
 
 function nulo() {
@@ -44,25 +49,37 @@ const inputScreen = document.getElementById('input-screen')
 const confirmBtn = document.getElementById('confirm')
 
 confirmBtn.addEventListener('click', function()  {
-
+    
     if(canIStart === true){
-
-        if(input.value == 1) {
-            objeto['primeiraChapa'] += 1
+        
+        if(input.value == 14) {
+            objeto['fenix'] += 1
             localStorage.setItem('chapa', JSON.stringify(objeto))
 
-        } else if(input.value == 2){
-            objeto['segundaChapa'] += 1
+        } else if(input.value == 7){
+            objeto['rep'] += 1
             localStorage.setItem('chapa', JSON.stringify(objeto))
 
+        } else if(input.value == 54){
+            objeto['fazerADiferenca'] += 1
+            localStorage.setItem('chapa', JSON.stringify(objeto))
+        } else if(input.value == 23) {
+            objeto['estudantesEmAcao'] += 1
+            localStorage.setItem('chapa', JSON.stringify(objeto))
+        } else if(input.value == 67) {
+            objeto['lionBlack'] += 1
+            localStorage.setItem('chapa', JSON.stringify(objeto))
         } else {
             return alert("NÃO TEMOS UMA CHAPA COM ESSE NÚMERO : " + input.value)
-        }    
+        } 
+        expression = ''
+        input.value = expression
         canIStart = false
         inputScreen.style.display = 'none'
     } 
     
     setTimeout(again, 5000)
+    
     console.log(objeto)
     
 })
